5cf8bd2b4b672a96c75af5dc5f2e26bb90cf752b
jun0S2 <leen6225@gmail.com>
2025-12-10 02:09:57 +0100

    enhance previous solution

--- 코딩기초트레이닝/마지막두원소.py
+++ 코딩기초트레이닝/마지막두원소.py
@@ -1,11 +1,16 @@
 # .append()
 # last element : num_list[-1]
+# def solution(num_list):
+#     answer = num_list
+#     # last_element = num_list[-1];
+#     # print(last_element)
+#     if num_list[-1] > num_list[-2] :
+#         answer.append(num_list[-1]-num_list[-2])
+#     else :
+#         answer.append(num_list[-1]*2)
+#     return answer
+
 def solution(num_list):
-    answer = num_list
-    # last_element = num_list[-1];
-    # print(last_element)
-    if num_list[-1] > num_list[-2] :
-        answer.append(num_list[-1]-num_list[-2])
-    else :
-        answer.append(num_list[-1]*2)
-    return answer
+    last, prev = num_list[-1], num_list[-2]
+    append_val = last - prev if last > prev else last * 2
+    return num_list + [append_val]
